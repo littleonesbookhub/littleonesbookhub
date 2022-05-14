a45ac35ebb3b2715df7fafe6f30a1782f2775c71
priya <priya00020130@gmail.com>
2022-05-14 17:47:39 +0530

    search page navbar

---
+++ js/common.js
@@ -0,0 +1,21 @@
+function register_navbar_menu_button_click_handler() {
+    const navbar_menu_button = document.getElementsByClassName("navbar-menu")[0];
+    navbar_menu_button.addEventListener("click", on_navbar_menu_button_click);
+}
+
+function on_navbar_menu_button_click() {
+    const mobile_nav_links = document.getElementById("mobile-nav-links")
+    const mobile_nav = document.querySelector('.mobile-nav')
+    if (mobile_nav_links.style.display === "block") {
+        mobile_nav.classList.remove("mobile-nav-open");
+        mobile_nav_links.style.display = "none";
+    }
+    else {
+        mobile_nav.classList.add("mobile-nav-open");
+        mobile_nav_links.style.display = "block";
+    }
+}
+
+function on_page_load_common() {
+    register_navbar_menu_button_click_handler();
+}
---
+++ js/search.js
@@ -0,0 +1,5 @@
+function on_page_load() {
+    on_page_load_common();
+}
+
+window.onload = on_page_load;
